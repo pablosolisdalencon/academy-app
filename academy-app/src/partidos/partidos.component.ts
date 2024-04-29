3e94f9e68f7a8b5f8a5d1d2b4a3e6f7d21d87d05
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2024-04-29 00:36:03 -0400

    complete skeleton

---
+++ academy-app/src/partidos/partidos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partidos',
+  standalone: true,
+  imports: [],
+  templateUrl: './partidos.component.html',
+  styleUrl: './partidos.component.css'
+})
+export class PartidosComponent {
+
+}
