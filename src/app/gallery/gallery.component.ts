c76ba6778f252d9f43a0c14dd63b82aacd76cdf8
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2024-05-28 01:21:15 -0400

    relink inscribe button

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,9 +9,18 @@ import { MaterialModule } from '../../modules/material/material.module';
 })
 export class GalleryComponent {
   galleryImages = [
-    { src: 'path/to/image1.jpg', alt: 'Image 1' },
-    { src: 'path/to/image2.jpg', alt: 'Image 2' },
-    { src: 'path/to/image3.jpg', alt: 'Image 3' },
+    
+    { src: 'assets/logo0.jpg', alt: 'Logo 0' },
+    { src: 'assets/logo1.jpg', alt: 'Logo 1' },
+    { src: 'assets/evento0.jpg', alt: 'Evento 0' },
+    { src: 'assets/evento0-b.jpg', alt: 'Evento 0b' },
+    { src: 'assets/foto0.jpg', alt: 'Image 0' },
+    { src: 'assets/foto1.jpg', alt: 'Image 1' },
+    { src: 'assets/foto2.jpg', alt: 'Image 2' },
+    { src: 'assets/foto3.jpg', alt: 'Image 3' },
+    { src: 'assets/foto4.jpg', alt: 'Image 4' },
+    { src: 'assets/foto5.jpg', alt: 'Image 5' },
+    { src: 'assets/foto6.jpg', alt: 'Image 6' }
     // Add more images as needed
   ];
 }
